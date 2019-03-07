94f51ed63f7fbf043d9bfa893110365d720c0c9e
nyx-coder <kadamshailesh007@gmail.com>
2019-03-07 13:24:14 +0530

    initial

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, { Component } from "react";
+import Dashboard from "./src/App/Dashboard";
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Dashboard />
+      </React.Fragment>
+    );
+  }
+}
